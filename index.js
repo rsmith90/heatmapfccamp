a0f42ea864efdf8dc8f42ac35e260881ad29ca64
rsmith90 <burt2speed@gmail.com>
2022-04-09 18:30:14 -0400

    added bottom axis

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
-let url =
-  "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
-let req = new XMLHttpRequest();
+let url = "https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json";
+let req = new XMLHttpRequest()
 
-let values = [];
-let baseTemp;
+let values = []
+let baseTemp
 
-let xScale;
-let yScale;
+let xScale
+let yScale
 
-let yAxis;
-let xAxis;
+let yAxis
+let xAxis
 
-let width = 800;
-let height = 600;
-let padding = 40;
+let width = 800
+let height = 600
+let padding = 40
 
-let svg = d3.select("svg");
+let svg = d3.select("svg")
 
-let drawCanvas = () => {
-  svg.attr("width", width);
-  svg.attr("height", height);
+let canvas = d3.select('#canvas')
+  svg.attr("width", width)
+  svg.attr("height", height)
+
+let generateScales = () => {
+
+  xScale = d3.scaleLinear()
+    .range([padding, width-padding])
 };
 
-let generateScale = () => {};
+let drawCells = () => {};
 
-let heatPoints = () => {};
+let drawAxes = () => {
+  let xAxis = d3.axisBottom(xScale)
 
-let drawAxis = () => {};
+  canvas.append('g')
+    .call(xAxis)
+    .attr('id', 'x-axis')
+    .attr('transform', 'translate(0, ' + (height-padding) +')')
+};
 
-req.open("GET", url, true);
+req.open('GET', url, true)
 req.onload = () => {
-  let values = JSON.parse(req.responseText);
-  baseTemp = object["baseTempreature"];
-  values = object["monthlyVariance"];
-  console.log(values);
+  let object = JSON.parse(req.responseText);
+  baseTemp = object['baseTempreature'];
+  values = object['monthlyVariance'];
   console.log(baseTemp);
-  drawCanvas();
-  generateScale();
-  heatPoints();
-  drawAxis();
+  console.log(values);
+  generateScales();
+  drawCells();
+  drawAxes();
 };
 req.send();
